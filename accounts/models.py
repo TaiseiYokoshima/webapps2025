35e7f76ed4bb8394c119f5edd58eefaee543e70a
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-03-18 20:33:50 +0000

    added balance and default accessing method

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
-# Create your models here.
+from decimal import Decimal
 
 
 class Account(AbstractUser):
-    CURRENCIES = [ 
-        ("GBP", "GBP"),
-        ("USD", "USD"), 
-        ("EUR", "EUR")
-    ]
+    CURRENCIES = { 
+        "GBP": "GBP",
+        "USD": "USD", 
+        "EUR": "EUR"
+    }
 
     email = models.EmailField(unique=True, primary_key=True, null=False, blank=False)
-    currency = models.CharField(max_length=3, choices=CURRENCIES, null=False, blank=False)
+    currency = models.CharField(max_length=3, choices=CURRENCIES, null=False, blank=False, default="GBP")
     username = models.CharField(max_length=1, blank=True, null=True, unique=True)
+    balance = models.DecimalField(max_digits=50, decimal_places=2, default=Decimal("750.00"), null=False, blank=False)
+
+
+    def get_default(field_name: str):
+        return Account._meta.get_field(field_name).default
     
 
