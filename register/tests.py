fd7c557f781c3776e3212f0b4fcc2c65a5a3f537
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-03-19 23:23:32 +0000

    tested register endpoints in case the conversion api does not work

--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,180 @@
-from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+
+
+
+from .forms import RegisterForm
+
+from .views import register_user_from_form
+
+
+
+from utils.test_utils import LiveServerBase
+
+import services.currency as glob
+
+from requests.exceptions import ConnectTimeout
+
+
+
+from services.currency import ConversionAPIError
+
+
+from accounts.models import Account
+
+
+from decimal import Decimal
+
+
+from utils.currency import CurrencyAmount
+from conversion.views import rates
+
+
+
+from http import HTTPStatus
+
+
+
+class TestHandleCurrencyChoice(LiveServerBase):
+    def setUp(self):
+        super().setUp()
+        password = "!123123Jj"
+        form_data = {
+            "email": "taisei@test.com",
+            "password1": password,
+            "password2": password,
+            "currency": "EUR"
+        }
+
+        form: RegisterForm = RegisterForm(data=form_data)
+
+        form.is_valid()
+
+        self.form = form
+
+
+    def test_handle_currency_choice_external_api(self):
+        glob.external_api_url = "example.com"
+        request = self.create_request_not_secure()
+        
+        with self.assertRaises(ConversionAPIError):
+            register_user_from_form(request, self.form)
+
+        glob.external_api_url = "12.1.1.1"
+        with self.assertRaises(ConnectTimeout):
+            register_user_from_form(request, self.form)
+
+
+    def test_handle_currency_choice_internal_api(self):
+        request = self.create_request_not_secure()
+        user = register_user_from_form(request, self.form)
+        self.assertTrue(isinstance(user, Account))
+
+
+    def test_currency_conversions(self):
+        request = self.create_request_not_secure()
+        user = register_user_from_form(request, self.form)
+        balance = user.balance
+
+
+        
+        source = "GBP"
+        target = self.form.cleaned_data["currency"]
+        rate = rates[source][target]
+
+
+        amount = Account.get_default("balance")
+
+        calculated = (CurrencyAmount(amount) * CurrencyAmount(rate)).into()
+        calculated =  Decimal(calculated)
+
+
+        self.assertEqual(balance, calculated)
+
+
+        user = Account.objects.get(email__exact=self.form.cleaned_data["email"])
+        balance = user.balance
+
+        self.assertEqual(balance, calculated)
+
+
+
+class TestRegisterEndPoint(LiveServerBase):
+    def setUp(self):
+        self.client = Client()
+
+
+
+    def make_get_request(self):
+        response = self.client.get("/register/")
+        self.csrf_token = getattr(response, "cookies")["csrftoken"].value
+        self.password =  "!123123Jj"
+        self.form_data = {
+            "csrfmiddlewaretoken": self.csrf_token, 
+            "email": "taisei@icloud.com",
+            "currency": "EUR",
+            "password1": self.password,
+            "password2": self.password
+        }
+
+
+
+        return response
+
+
+    def test_register(self):
+        self.make_get_request()
+        self.client.post("/register/", self.form_data, headers={"Referrer": self.live_server_url + "/register/"})
+        Account.objects.get(email__exact="taisei@icloud.com")
+        self.assertTrue("_auth_user_id" in self.client.session)
+
+
+    def test_external_api_ip(self):
+        response = self.make_get_request()
+        glob.external_api_url = "12.1.1.1" 
+        response = self.client.post("/register/", self.form_data, headers={"Referrer": self.live_server_url + "/register/"})
+        self.assertEqual(getattr(response,"status_code"), HTTPStatus.INTERNAL_SERVER_ERROR)
+
+        with self.assertRaises(getattr(Account, "DoesNotExist")):
+            Account.objects.get(email__exact="taisei@icloud.com")
+
+
+
+    def test_external_api_domain(self):
+        response = self.make_get_request()
+        glob.external_api_url = "example.com" 
+        response = self.client.post("/register/", self.form_data, headers={"Referrer": self.live_server_url + "/register/"})
+        self.assertEqual(getattr(response,"status_code"), HTTPStatus.INTERNAL_SERVER_ERROR)
+
+        with self.assertRaises(getattr(Account, "DoesNotExist")):
+            Account.objects.get(email__exact="taisei@icloud.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
--- register/views.py
+++ register/views.py
@@ -1,74 +1,64 @@
+from django.http.response import HttpResponseServerError
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
 
 
 from . forms import RegisterForm
+from accounts.models import Account
+from services.currency import call_conversion_api
+
+
+from decimal import Decimal
+
 
 from accounts.models import Account
 
 
-from services.currency import call_conversion_api
+def register_user_from_form(request, form: RegisterForm):
+    default_currency = Account.get_default("currency")
+    chosen_currency = form.cleaned_data["currency"]
 
-
-
-def handle_currency_choice(request, form: RegisterForm):
-    default = Account.get_default("currency")
-    currency = form.cleaned_data["currency"]
-
-    if (default == currency):
+    if (default_currency == chosen_currency):
         user = form.save()
         return user
 
-    
-    amount = "750.00"
-
-
-    try:
-        result = call_conversion_api(request, default, currency, amount)
-    except ValueError as e:
-        message = str(e)
-        
+    email = form.cleaned_data["email"]
+    default_balance = str(Account.get_default("balance"))
+    balance = call_conversion_api(request, default_currency, chosen_currency, default_balance)
+    password = form.cleaned_data["password1"]
 
 
 
-
-
-
-
-
-
-
-
+    user = Account(email=email, balance=Decimal(balance), currency=chosen_currency)
+    user.set_password(password)
+    user.save()
     
 
-
-
-
-
-
-
-
-
+    return user
 
 
 
 
 
 def register(request):
-    # print(Account._meta.get_field("currency").default)
-    print(Account.get_default("email"))
-
     if request.method == 'POST':
-        print("\n\npost data:\n" + str(request.POST), end="\n\n")
         form = RegisterForm(request.POST)
 
 
         if form.is_valid():
-            user = handle_currency_choice(request, form)
+            try:
+                user = register_user_from_form(request, form)
+            except Exception:
+                return HttpResponseServerError("Internal server error, try again later")
+
+
             login(request, user)
             return redirect('home')
 
+
+        print("\n\n\n\nform was invalid:")
+        print(form.errors.as_data(), end="\n\n\n\n")
     else:
         form = RegisterForm()
     
